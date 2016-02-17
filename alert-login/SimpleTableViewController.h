8a15fb55b4cc4978a22bd4ad80e9b7da1f38493c
Amogh J S <amogh.js@neorays.com>
2016-02-17 20:48:55 +0530

    Displays a table view with menu items

---
+++ alert-login/SimpleTableViewController.h
@@ -0,0 +1,12 @@
+//
+//  SimpleTableViewController.h
+//  SimpleTable
+//
+
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTableViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
