98e677573f4e6a54b2d9cad77fb781d8378969c3
BhagyashreeKandakur <test@tests-Mac.local>
2016-03-30 09:26:32 -0200

    UIButton and Label App
    Displays the message in label when the UIButton is clicked

---
+++ Bhagyashree/VeiwColourApp/VeiwColourApp/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  VeiwColourApp
+//
+//  Created by test on 3/30/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *helloLabel;
+
+
+- (IBAction)pressButton:(id)sender;
+- (IBAction)smileButton:(id)sender;
+
+@end
+
