d6f0fff889d64f7188d7630d4888779e1a464ee1
Harishakn <test@tests-Mac.local>
2016-04-04 09:40:45 -0200

    IOS Label Demo
    IOS APP

---
+++ Harisha/LabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LabelDemo
+//
+//  Created by test on 3/26/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Harisha/LabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LabelDemo
+//
+//  Created by test on 3/26/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
