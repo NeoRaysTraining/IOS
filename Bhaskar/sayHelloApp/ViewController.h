04a366f70dab302943942dcf8af1089bde6ef134
bhaskarns <test@tests-Mac.local>
2016-04-04 11:12:15 +0530

    button with label
    label action with button click

---
+++ Bhaskar/sayHelloApp/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  sayHelloApp
+//
+//  Created by test on 3/26/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *myTitleLabel;
+- (IBAction)Press:(id)sender;
+
+@end
+
