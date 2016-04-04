e2b2e62f405d5efb4dd3370d6a82882a15b39408
bhaskarns <test@tests-Mac.local>
2016-04-04 11:09:10 +0530

    Button Demo
    button Demonstration

---
+++ Bhaskar/ButtonDemo/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  ButtonDemo
+//
+//  Created by test on 3/30/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *helloLabel;
+- (IBAction)helloButton:(id)sender;
+
+- (IBAction)doNothin:(id)sender;
+
+
+@end
+
