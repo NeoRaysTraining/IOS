6f8a7734efe4960ab1db60def27c05ab86c21678
Mahaboobsab Nadaf <mahaboobsab.n@neorays.com>
2016-04-07 18:53:30 +0530

    iOS App example using Autolayouts
    iOS App example using Autolayouts

---
+++ Meheboob/iOS Programs/Autolayoutexample/Autolayoutexample4/Autolayoutexample4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Autolayoutexample4
+//
+//  Created by test on 4/7/16.
+//  Copyright © 2016 com.NeoRays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
