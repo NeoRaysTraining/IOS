3629213348119d156680471a9baa80c1351d8b31
BhagyashreeKandakur <test@tests-Mac.local>
2016-04-07 10:14:20 -0200

    Tap Game App
    Tap Game App counts the no. of taps and displays the result

---
+++ Bhagyashree/TapGameApp/TapGameApp/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  TapGameApp
+//
+//  Created by test on 4/6/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)pressButton:(id)sender;
+
+@end
+
---
+++ Bhagyashree/TapGameApp/TapGameApp/ViewController.m
@@ -0,0 +1,64 @@
+//
+//  ViewController.m
+//  TapGameApp
+//
+//  Created by test on 4/6/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AppDelegate.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *enterTextField;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property NSString *enterValue;
+@property (weak, nonatomic) IBOutlet UIButton *pressButton;
+
+@end
+
+@implementation ViewController
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   //  Do any additional setup after loading the view, typically from a nib.
+
+    [self.messageLabel setHidden:YES];
+    [self.resultLabel setHidden:YES];
+    [self.pressButton setHidden:YES];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    self.enterValue = self.enterTextField.text;
+    [self.messageLabel setHidden:NO];
+    [self.pressButton setHidden:NO];
+    [self.messageLabel setText:[NSString stringWithFormat:@"Tap Button %@ times",self.enterValue]];
+    [self.enterTextField setKeyboardType:UIKeyboardTypeDefault];
+    [self.enterTextField setHidden:YES];
+    return YES;
+}
+static int taps=0;
+- (IBAction)pressButton:(id)sender {
+    taps++;
+    int var;
+    var = [_enterValue intValue];
+   if(taps==var)
+    {
+        [self.messageLabel setHidden:YES];
+        [self.pressButton setHidden:YES];
+        [self.enterTextField setHidden:YES];
+        [self.resultLabel setHidden:NO];
+        [self.resultLabel setText:@"YOU WON"];
+        taps=0;
+    }
+}
+@end
