40ae6a009f14edd485e3926fcfb031284d338313
BhagyashreeKandakur <test@tests-Mac.local>
2016-04-14 02:18:17 -0200

    Text Field example using array and dictionary
    Text filed example using array and dictionary

---
+++ Bhagyashree/TextFieldWithArrayDictionary/TextFieldWithArrayDictionary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextFieldWithArrayDictionary
+//
+//  Created by test on 4/1/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
