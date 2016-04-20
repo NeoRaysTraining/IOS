5046f56fe3c028acc98469be248848294e6c582f
BhagyashreeKandakur <test@tests-Mac.local>
2016-04-20 10:53:05 -0200

    Push Segue and Image View Example
    Push Segue and Image View Example

---
+++ Bhagyashree/SegueImageDisplayEx/SegueImageDisplayEx/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SegueImageDisplayEx
+//
+//  Created by test on 4/19/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *enterField;
+
+
+@end
+
