2973fc53da177902e3b8965a4d4ac8212aea9558
Meheboob <test@tests-Mac.local>
2016-04-25 18:26:59 +0530

    Table View
    An iOS App using Table view

---
+++ Meheboob/iOS Programs/TableView/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TableViewController.h
+//  TableView
+//
+//  Created by test on 25/04/16.
+//  Copyright © 2016 com.NeoRays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+@property (strong, nonatomic) NSArray* textArray;
+
+@property (strong, nonatomic) NSArray *textArrayElements;
+
+@end
---
+++ Meheboob/iOS Programs/TableView/TableViewController.m
@@ -0,0 +1,104 @@
+//
+//  TableViewController.m
+//  TableView
+//
+//  Created by test on 25/04/16.
+//  Copyright © 2016 com.NeoRays. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+@interface TableViewController ()
+
+@end
+
+@implementation TableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
+    self.textArray = [[NSArray alloc]initWithObjects:@"APj1",@"Apj2",@"Apj3",@"Apj5",@"Apj5", nil];
+    
+    self.textArrayElements = [[NSArray alloc]initWithObjects:@"1111",@"2222",@"3333",@"4444",@"5555", nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return [_textArray count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    cell.imageView.image = [UIImage imageNamed:[self.textArray objectAtIndex:indexPath.row] ];
+    
+    
+    cell.textLabel.text = [self.textArray objectAtIndex:indexPath.row];
+    cell.detailTextLabel.text = [self.textArrayElements objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
