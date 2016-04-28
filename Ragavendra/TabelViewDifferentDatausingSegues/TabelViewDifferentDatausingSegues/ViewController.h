6be845af72771bb4f20e650d6b53e9108b51e752
raghavendradattawad <raghavendra.d@neorays.com>
2016-04-28 11:23:45 -0200

    IOS App UITAbelView Using Different Fielld with segue
    By using table View send data to next scene

---
+++ Ragavendra/TabelViewDifferentDatausingSegues/TabelViewDifferentDatausingSegues/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TabelViewDifferentDatausingSegues
+//
+//  Created by test on 4/28/16.
+//  Copyright © 2016 NeoRays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController :UIViewController
+@end
+
---
+++ Ragavendra/TabelViewDifferentDatausingSegues/TabelViewDifferentDatausingSegues/ViewController2.h
@@ -0,0 +1,18 @@
+//
+//  ViewController2.h
+//  TabelViewDifferentDatausingSegues
+//
+//  Created by test on 4/28/16.
+//  Copyright © 2016 NeoRays. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ViewController.h"
+@interface ViewController2 : ViewController
+
+
+
+
+
+
+@end
