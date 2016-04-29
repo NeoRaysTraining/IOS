cb867e1ce3d2e488758109a41a5a049b92fdd92a
bhaskarns <test@tests-Mac.local>
2016-04-29 18:54:32 +0530

    UICollectionView
    showing details in grid view using UICollectionView

---
+++ Bhaskar/UIExamples/CollectionViewDemo/CollectionViewDemo/CollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.h
+//  CollectionViewDemo
+//
+//  Created by test on 4/29/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *imgView;
+@property (strong, nonatomic) IBOutlet UILabel *topicNameLbl;
+
+@end
