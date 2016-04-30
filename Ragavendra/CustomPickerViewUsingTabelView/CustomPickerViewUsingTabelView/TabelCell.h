01f46140fcebcd34106889e298d43a9cb5b8a768
raghavendradattawad <raghavendra.d@neorays.com>
2016-04-30 09:10:11 -0200

    CustomPickerView Using Tableview

---
+++ Ragavendra/CustomPickerViewUsingTabelView/CustomPickerViewUsingTabelView/TabelCell.h
@@ -0,0 +1,25 @@
+//
+//  TabelCell.h
+//  CustomPickerViewUsingTabelView
+//
+//  Created by test on 4/27/16.
+//  Copyright © 2016 NeoRays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabelCell : UITableViewCell
+
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *ageLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *countryLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *genderLabel;
+
+
+
+@end
