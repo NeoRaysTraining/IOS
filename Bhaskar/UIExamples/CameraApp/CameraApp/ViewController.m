415a380b0cfa1403a0e8689b9cdf639363a53c50
bhaskarns <test@tests-Mac.local>
2016-04-30 16:52:04 +0530

    Demo
    this will show camera device is not exist

---
+++ Bhaskar/UIExamples/CameraApp/CameraApp/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  CameraApp
+//
+//  Created by test on 4/28/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+- (IBAction)takePhoto:(id)sender {
+    
+    UIImagePickerController *picker=[[UIImagePickerController alloc]init];
+    picker.delegate=self;
+    picker.allowsEditing=YES;
+    picker.sourceType=UIImagePickerControllerSourceTypeCamera;
+    [self presentViewController:picker animated:YES completion:NULL];
+}
+- (IBAction)selectPhoto:(id)sender {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    UIImage *choosenImage=info[UIImagePickerControllerEditedImage];
+    self.imageView.image=choosenImage;
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                              message:@"Device has no camera"
+                                                             delegate:nil
+                                                    cancelButtonTitle:@"OK"
+                                                    otherButtonTitles: nil];
+        
+        [myAlertView show];
+        
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
