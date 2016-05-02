12a7abbf35e67c747ed57fb505b5c273dc84de2b
bhaskarns <test@tests-Mac.local>
2016-05-02 18:41:18 +0530

    wedgetDemo
    it shows the message in notification

---
+++ Bhaskar/UIExamples/WedgetDemo/new/TodayViewController.h
@@ -0,0 +1,15 @@
+//
+//  TodayViewController.h
+//  new
+//
+//  Created by test on 5/2/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+@property (strong,nonatomic)IBOutlet UIProgressView *barView;
+@property (strong, nonatomic) IBOutlet UILabel *labelPercent;
+
+@end
