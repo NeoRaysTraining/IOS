a29d8317c87cbe45168c56384137d8449d7a9960
Meheboob <test@tests-Mac.local>
2016-06-14 10:05:52 +0530

    Reachability
    An iOS program to check internet availability

---
+++ Meheboob/iOS Programs/Reachability/Reachability/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Reachability
+//
+//  Created by Mahaboobsab Nadaf on 13/06/16.
+//  Copyright © 2016 com.NeoRays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Reachability.h"
+@interface ViewController : UIViewController
+
+- (IBAction)checkNetConnection:(id)sender;
+
+@end
+
---
+++ Meheboob/iOS Programs/Reachability/Reachability/ViewController.m
@@ -0,0 +1,53 @@
+//
+//  ViewController.m
+//  Reachability
+//
+//  Created by Mahaboobsab Nadaf on 13/06/16.
+//  Copyright © 2016 com.NeoRays. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (void)checkForNetwork
+{
+    // check if we've got network connectivity
+    Reachability *myNetwork = [Reachability reachabilityWithHostName:@"google.com"];
+    NetworkStatus myStatus = [myNetwork currentReachabilityStatus];
+    
+    switch (myStatus) {
+        case NotReachable:
+            NSLog(@"There's no internet connection at all. Display error message now.");
+            break;
+            
+        case ReachableViaWWAN:
+            NSLog(@"We have a 3G connection");
+            break;
+            
+        case ReachableViaWiFi:
+            NSLog(@"We have WiFi.");
+            break;
+            
+        default:
+            break;
+    }
+}
+- (IBAction)checkNetConnection:(id)sender {
+    
+    [self checkForNetwork];
+}
+@end
