15a8fdf4a808b5c1a91b1cf760a2922ac24721b6
Harishakn <test@tests-Mac.local>
2016-06-20 05:59:31 -0200

    AEExchange Application

---
+++ Harisha/AEExchange/AEExchange/AEAssessmentResultVC.h
@@ -0,0 +1,13 @@
+//
+//  AEAssessmentResultVC.h
+//  AEExchange
+//
+//  Created by test on 6/18/16.
+//  Copyright © 2016 NeoRays. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface AEAssessmentResultVC : ViewController
+
+@end
---
+++ Harisha/AEExchange/AEExchange/AEAssessmentResultVC.m
@@ -0,0 +1,43 @@
+//
+//  AEAssessmentResultVC.m
+//  AEExchange
+//
+//  Created by test on 6/18/16.
+//  Copyright © 2016 NeoRays. All rights reserved.
+//
+
+#import "AEAssessmentResultVC.h"
+
+@interface AEAssessmentResultVC ()
+- (IBAction)produceCommLetter:(id)sender;
+
+@end
+
+@implementation AEAssessmentResultVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.automaticallyAdjustsScrollViewInsets=YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)produceCommLetter:(id)sender
+{
+    [self performSegueWithIdentifier:@"letterDetails" sender:self];
+}
+@end
